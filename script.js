174e60a12fb2f8d377004f892807aba7964a68a9
kundana-git <nagakundanamarrikanti@gmail.com>
2026-06-27 17:57:25 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+//clock
+setInterval(()=>{
+    document.getElementById("time").textContent=new Date().toLocaleTimeString();
+}, 1000);
+
+//To Do
+function add(){
+    let task=document.getElementById("task").value;
+    if(task){
+        document.getElementById("list").innerHTML+='<li>${task}</li>';
+        document.getElementById("task").value="";
+    }
+}
